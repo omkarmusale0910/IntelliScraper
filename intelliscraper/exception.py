e53f131e8df5d46e19a4f339eeffa3bf6268050f
omkar <omkarmusaleich@gmail.com>
2025-10-16 03:52:14 +0530

    removed unused exception

--- intelliscraper/exception.py
+++ intelliscraper/exception.py
@@ -1,9 +1,3 @@
-class ScrapError(Exception):
-    """Custom exception raised when scraping a URL fails."""
-
-    pass
-
-
 class HTMLParserInputError(ValueError):
     """Raised when the input provided to HTMLParser is invalid."""
 
